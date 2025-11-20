7e22ba312625618cf6c540e90200c02406ae456f
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-11-20 09:27:35 +0800

    generalize dupe to clean script

--- aiken_clean.py
+++ aiken_clean.py
@@ -1,6 +1,6 @@
 """
-Remove duplicate Aiken questions
-================================
+Remove duplicated and malformatted Aiken questions
+=================================================
 """
 
 import re
@@ -14,11 +14,20 @@ def parse_file(file_name):
     return [(q + a).strip() for q, a in zip(pairs[::2], pairs[1::2])]
 
 
+def non_empty_question(problem):
+    question = problem.split("A)", 1)[0]
+    return bool(question)
+
+
 if __name__ == "__main__":
 
     file_names = sys.argv[1:]
+
     questions = [parse_file(file_name) for file_name in file_names]
     questions = sum(questions, [])
-    unique = set(questions)
 
-    print(unique)
+    unique = set(questions)
+    complete = [u for u in unique if non_empty_question(u)]
+
+    for c in complete:
+        print(c)
